8af66a973aa6905e88632244f742e3fcdfa452a4
Nalbertsan <nalbert1000@gmail.com>
2023-01-19 16:23:06 -0300

    Pequenas mudanças em como trabalhar com o css

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,21 +7,25 @@ function main() {
         const peso = parseFloat(document.querySelector('#text1').value);
         const altura = parseFloat(document.querySelector('#text2').value);
         const imc = peso / (altura * altura);
-        const resultado = document.querySelector('#imcResultado');
+        const resultado = document.querySelector('#resultado');
+        const p = document.createElement('p');
         let faixaIMC = '';
-        if(imc < 18.5)
+        resultado.innerHTML = '';
+        p.classList.add('resultado_IMC');
+        resultado.appendChild(p);
+        if (imc < 18.5)
             faixaIMC = 'Abaixo do peso';
-        else if(imc >= 18.5 && imc < 25)
+        else if (imc >= 18.5 && imc < 25)
             faixaIMC = 'Peso normal';
-        else if(imc >= 25 && imc < 30)
+        else if (imc >= 25 && imc < 30)
             faixaIMC = 'Sobrepeso';
-        else if(imc >= 30 && imc < 35)
+        else if (imc >= 30 && imc < 35)
             faixaIMC = 'Obesidade grau 1';
-        else if(imc >= 35 && imc < 40)
+        else if (imc >= 35 && imc < 40)
             faixaIMC = 'Obesidade grau 2';
-        else if(imc >= 40)
+        else if (imc >= 40)
             faixaIMC = 'Obesidade grau 3';
-        resultado.innerHTML =  `Seu IMC é: ${imc.toFixed(2)} (${faixaIMC})`;
+        p.innerHTML = `Seu IMC é: ${imc.toFixed(2)} (${faixaIMC})`;
     }
     conteiner.addEventListener('submit', recebeEvento);
 }
